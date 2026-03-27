8bcbb282fdee0a29a1ed4ca20a0f95522f587067
Dedes <federico.lepore73@gmail.com>
2026-03-27 12:17:05 +0100

    Screenshots

--- src/main/java/federicolepore/Application.java
+++ src/main/java/federicolepore/Application.java
@@ -1,8 +1,18 @@
 package federicolepore;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4w16d5");
+
     public static void main(String[] args) {
+
+        EntityManager em = emf.createEntityManager();
+
+
         System.out.println("Hello World!");
     }
 }
